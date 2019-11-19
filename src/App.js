97e5a7ba1d77518bed748a590cc3c05733f6a3d3
adrian hernandez <adrianhh1426@gmail.com>
2019-11-18 19:57:49 -0600

    consumiendo api de noticias

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
 import React,{Component} from 'react';
 
 class App extends Component {
-  state = {};
-  
+  state = {
+    noticias:[]
+  };
+
+  componentDidMount(){
+    this.consultarNoticias();
+  }
+
+  consultarNoticias = async () =>{
+    const url = `https://newsapi.org/v2/top-headlines?country=mx&category=business&apiKey=329bd826caaf4fe0a15c4319e251df20`;
+
+    const respuesta = await fetch(url);
+    const noticias = await respuesta.json();
+
+    this.setState({
+      noticias : noticias.articles
+    });
+  }
+ 
   render() {
     return (
       <div>
